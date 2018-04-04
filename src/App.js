46f6439471521992d7877d03a09e08fc09ee43fc
Abhi <abhilashmalhotra17@gmail.com>
2018-04-04 21:39:37 +0530

    Passing Method ref

--- src/App.js
+++ src/App.js
@@ -6,28 +6,46 @@ import './App.css';
 class App extends Component {
   state = {
     persons: [
-      {name: 'Abhilash', age: 25},
-      {name: 'Vicky', age: 28},
-      {name: 'Nitin', age: 24},
-      {name: 'Inder', age: 30},
+    {name: 'Abhilash', age: 25},
+    {name: 'Vicky', age: 28},
+    {name: 'Nitin', age: 24},
+    {name: 'Inder', age: 30},
     ]
   }
 
-  switchNameHandler = () => {
-    console.log('Button Clicked');
+  switchNameHandler = (myName) => {
+    //console.log('Button Clicked');
+    //this.state.persons[0].name = 'Abhilash Malhotra';
+    this.setState({
+      persons: [
+      {name: myName, age: 25},
+      {name: 'Vicky', age: 28},
+      {name: 'Nitin', age: 24},
+      {name: 'Inder', age: 40},
+      ]
+    });
   }
 
   render() {
    // return React.createElement('div',{className: 'center'},React.createElement('h1',null,'Hello Abhilash'));
    return (
-      <div className="center">
-        <h1>Hello Abhilash</h1>
-        <button onClick={this.switchNameHandler}>Switch</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
-      </div>
+    <div className="center">
+    <h1>Hello Abhilash</h1>
+    <button onClick={this.switchNameHandler}>Switch</button>
+    <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age} />
+    <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, "Abhilash!!")} />
+    <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}></Person>
+    <Person 
+        name={this.state.persons[3].name} 
+        age={this.state.persons[3].age} />
+    </div>
     );
   }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -14,7 +14,7 @@ class Person extends Component{
 	render(){
 		return (
 			<div>
-				<h1>Hi I am {this.props.name}, and I am {this.props.age} Year Old</h1>
+				<h1 onClick={this.props.click}>Hi I am {this.props.name}, and I am {this.props.age} Year Old</h1>
 				<p>{this.props.children}</p>
 			</div>
 			);
