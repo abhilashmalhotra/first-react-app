315752f951aafb77dc01151f7872e84be78c1334
Abhi <abhilashmalhotra17@gmail.com>
2018-04-04 21:05:32 +0530

    Event Handler

--- src/App.js
+++ src/App.js
@@ -12,11 +12,17 @@ class App extends Component {
       {name: 'Inder', age: 30},
     ]
   }
+
+  switchNameHandler = () => {
+    console.log('Button Clicked');
+  }
+
   render() {
    // return React.createElement('div',{className: 'center'},React.createElement('h1',null,'Hello Abhilash'));
    return (
       <div className="center">
         <h1>Hello Abhilash</h1>
+        <button onClick={this.switchNameHandler}>Switch</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
